f2d65071f82f8f7a2a660123956cfa5f0042b95e
Oleg Burov <oleg.burov@outlook.com>
2016-08-30 12:33:36 -0700

    Updated version number.

--- src/Sitecore.Support.391039/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.391039/Properties/AssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: AssemblyVersion("8.1.2.0")]
+[assembly: AssemblyFileVersion("8.1.2.0")]
+[assembly: AssemblyInformationalVersion("8.1.2 rev. 160830")]

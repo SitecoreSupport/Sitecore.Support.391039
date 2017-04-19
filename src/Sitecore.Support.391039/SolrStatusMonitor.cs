52e945545bf60edf0211e99555829aeca3741cba
dk\ivd <ivd@sitecore.net>
2017-04-19 17:46:17 +0300

    #391030: Rework SolrStatusMonitor

--- src/Sitecore.Support.391039/SolrStatusMonitor.cs
+++ src/Sitecore.Support.391039/SolrStatusMonitor.cs
@@ -23,91 +23,67 @@
                 return;
             }
 
-            var solrSearchIndex = index as SolrSearchIndex;
+            var solrSearchIndex = index as Sitecore.ContentSearch.SolrProvider.SolrSearchIndex;
             if (solrSearchIndex == null)
             {
                 return;
             }
 
+            var failResistantIndex = index as IFailResistantIndex;
+
+            if (failResistantIndex == null)
+            {
+                // the index doesn't support this feature, so the index is considered as always available
+                return;
+            }
+
             try
             {
-                var coreResult = solrAdmin.Status(solrSearchIndex.Core).FirstOrDefault();
-                if (coreResult.Index == null)
+                var newStatus = solrAdmin.Status(solrSearchIndex.Core).FirstOrDefault();
+
+                if (newStatus.Index == null)
                 {
                     throw new SolrConnectionException("SUPPORT: Core's index is null.");
                 }
 
-                var failResistantSolrSearchIndex2 = (solrSearchIndex as Sitecore.Support.ContentSearch.SolrProvider.SolrSearchIndex);
-                if (failResistantSolrSearchIndex2 != null)
+                if (failResistantIndex.ConnectionStatus == ConnectionStatus.Unknown)
                 {
-                    if (failResistantSolrSearchIndex2.PreviousConnectionStatus == ConnectionStatus.Unknown)
-                    {
-                        Log.Info("SUPPORT: Connection to [" + failResistantSolrSearchIndex2.Core + "] Solr core was established. [" + failResistantSolrSearchIndex2.Name + "] index is being initialized.", failResistantSolrSearchIndex2);
-                        failResistantSolrSearchIndex2.Initialize();
-                    }
-                    else if (failResistantSolrSearchIndex2.PreviousConnectionStatus == ConnectionStatus.Failed)
-                    {
-                        Log.Info("SUPPORT: Connection to [" + failResistantSolrSearchIndex2.Core + "] Solr core was restored.", failResistantSolrSearchIndex2);
-                    }
-
-                    failResistantSolrSearchIndex2.PreviousConnectionStatus = ConnectionStatus.Succeded;
+                    Log.Info(
+                        $"SUPPORT: Connection to [{solrSearchIndex.Core}] Solr core was established. [{solrSearchIndex.Name}] index is being initialized.",
+                        solrSearchIndex);
+                    solrSearchIndex.Initialize();
                 }
-                else
+                else if (failResistantIndex.ConnectionStatus == ConnectionStatus.Failed)
                 {
-                    var failResistantSwitchOnRebuildSolrSearchIndex = (solrSearchIndex as Sitecore.Support.ContentSearch.SolrProvider.SwitchOnRebuildSolrSearchIndex);
-                    if (failResistantSwitchOnRebuildSolrSearchIndex != null)
-                    {
-                        if (failResistantSwitchOnRebuildSolrSearchIndex.PreviousConnectionStatus == ConnectionStatus.Unknown)
-                        {
-                            Log.Info("SUPPORT: Connection to [" + failResistantSwitchOnRebuildSolrSearchIndex.Core + "] Solr core was established. [" + failResistantSwitchOnRebuildSolrSearchIndex.Name + "] index is being initialized.", failResistantSwitchOnRebuildSolrSearchIndex);
-                            failResistantSwitchOnRebuildSolrSearchIndex.Initialize();
-                        }
-                        else if (failResistantSwitchOnRebuildSolrSearchIndex.PreviousConnectionStatus == ConnectionStatus.Failed)
-                        {
-                            Log.Info("SUPPORT: Connection to [" + failResistantSwitchOnRebuildSolrSearchIndex.Core + "] Solr core was restored.", failResistantSwitchOnRebuildSolrSearchIndex);
-                        }
-
-                        failResistantSwitchOnRebuildSolrSearchIndex.PreviousConnectionStatus = ConnectionStatus.Succeded;
-                    }
+                    Log.Info($"SUPPORT: Connection to [{solrSearchIndex.Core}] Solr core was restored.", solrSearchIndex);
                 }
+
+                failResistantIndex.ConnectionStatus = ConnectionStatus.Succeded;
             }
             catch (SolrConnectionException ex)
             {
                 if (ex.Message.Contains("java.lang.IllegalStateException") && ex.Message.Contains("appears both in delegate and in cache"))
                 {
-                    Log.Warn("SUPPORT: Status check for [" + solrSearchIndex.Core + "] Solr core failed. Error suppressed as not related to Solr core availability. Details: https://issues.apache.org/jira/browse/LUCENE-7188", ex);
+                    Log.Warn(
+                        $"SUPPORT: Status check for [{solrSearchIndex.Core}] Solr core failed. Error suppressed as not related to Solr core availability. Details: https://issues.apache.org/jira/browse/LUCENE-7188",
+                        solrSearchIndex);
+                    Log.Debug($"SUPPORT: Solr exception\r\n{ex}", solrAdmin);
+
                     return;
                 }
 
-                Log.Warn("SUPPORT: Unable to connect to [" + SolrContentSearchManager.ServiceAddress + "], Core: [" + solrSearchIndex.Core + "]", ex, solrSearchIndex);
-                var failResistantSolrSearchIndex = (solrSearchIndex as Sitecore.Support.ContentSearch.SolrProvider.SolrSearchIndex);
-                if (failResistantSolrSearchIndex != null)
+                Log.Warn($"SUPPORT: Unable to connect to [{SolrContentSearchManager.ServiceAddress}], Core: [{solrSearchIndex.Core}]",
+                    ex, solrSearchIndex);
+
+                if (failResistantIndex.ConnectionStatus == ConnectionStatus.Succeded)
                 {
-                    if (failResistantSolrSearchIndex.PreviousConnectionStatus == ConnectionStatus.Succeded)
-                    {
-                        Log.Warn("SUPPORT: Connection to [" + failResistantSolrSearchIndex.Core + "] Solr core was lost.", failResistantSolrSearchIndex);
-                        failResistantSolrSearchIndex.PreviousConnectionStatus = ConnectionStatus.Failed;
-                    }
-                    else if (failResistantSolrSearchIndex.PreviousConnectionStatus == ConnectionStatus.Unknown)
-                    {
-                        Log.Warn("SUPPORT: Connection to [" + failResistantSolrSearchIndex.Core + "] Solr core was not established.", failResistantSolrSearchIndex);
-                    }                            
+                    Log.Warn($"SUPPORT: Connection to [{solrSearchIndex.Core}] Solr core was lost.", failResistantIndex);
+
+                    failResistantIndex.ConnectionStatus = ConnectionStatus.Failed;
                 }
-                else
+                else if (failResistantIndex.ConnectionStatus == ConnectionStatus.Unknown)
                 {
-                    var failResistantSwitchOnRebuildSolrSearchIndex = (solrSearchIndex as Sitecore.Support.ContentSearch.SolrProvider.SwitchOnRebuildSolrSearchIndex);
-                    if (failResistantSwitchOnRebuildSolrSearchIndex != null)
-                    {
-                        if (failResistantSwitchOnRebuildSolrSearchIndex.PreviousConnectionStatus == ConnectionStatus.Succeded)
-                        {
-                            Log.Warn("SUPPORT: Connection to [" + failResistantSwitchOnRebuildSolrSearchIndex.Core + "] Solr core was lost.", failResistantSwitchOnRebuildSolrSearchIndex);
-                            failResistantSwitchOnRebuildSolrSearchIndex.PreviousConnectionStatus = ConnectionStatus.Failed;
-                        }
-                        else if (failResistantSwitchOnRebuildSolrSearchIndex.PreviousConnectionStatus == ConnectionStatus.Unknown)
-                        {
-                            Log.Warn("SUPPORT: Connection to [" + failResistantSwitchOnRebuildSolrSearchIndex.Core + "] Solr core was not established.", failResistantSwitchOnRebuildSolrSearchIndex);
-                        }                                
-                    }
+                    Log.Warn($"SUPPORT: Connection to [{solrSearchIndex.Core}] Solr core was not established.", failResistantIndex);
                 }
             }
         }
@@ -128,25 +104,21 @@
             {
                 if (ex.Message.Contains("java.lang.IllegalStateException") && ex.Message.Contains("appears both in delegate and in cache"))
                 {
-                    Log.Warn("SUPPORT: Status check for [" + SolrContentSearchManager.ServiceAddress + "] Solr server failed. Error suppressed as not related to Solr core availability. Details: https://issues.apache.org/jira/browse/LUCENE-7188", ex);
+                    Log.Warn($"SUPPORT: Status check for [{SolrContentSearchManager.ServiceAddress}] Solr server failed. Error suppressed as not related to Solr core availability. Details: https://issues.apache.org/jira/browse/LUCENE-7188", solrAdmin);
+                    Log.Debug($"SUPPORT: Solr exception\r\n{ex}", solrAdmin);
+
                     return;
                 }
 
-                Log.Warn("SUPPORT: Unable to connect to [" + SolrContentSearchManager.ServiceAddress + "]. All Solr search indexes are unavailable.", ex, solrAdmin);
+                Log.Warn($"SUPPORT: Unable to connect to [{SolrContentSearchManager.ServiceAddress}]. All Solr search indexes are unavailable.", ex, solrAdmin);
+                Log.Debug($"SUPPORT: Solr exception\r\n{ex}", solrAdmin);
+
                 foreach (var index in SolrContentSearchManager.Indexes)
                 {
-                    var failResistantSolrSearchIndex = index as Sitecore.Support.ContentSearch.SolrProvider.SolrSearchIndex;
-                    if (failResistantSolrSearchIndex != null && failResistantSolrSearchIndex.PreviousConnectionStatus != ConnectionStatus.Unknown)
+                    var resistantIndex = index as IFailResistantIndex;
+                    if (resistantIndex != null && resistantIndex.ConnectionStatus != ConnectionStatus.Unknown)
                     {
-                        failResistantSolrSearchIndex.PreviousConnectionStatus = ConnectionStatus.Failed;
-                        continue;
-                    }
-
-                    var failResistantSwitchOnRebuildSolrSearchIndex = index as Sitecore.Support.ContentSearch.SolrProvider.SwitchOnRebuildSolrSearchIndex;
-                    if (failResistantSwitchOnRebuildSolrSearchIndex != null && failResistantSwitchOnRebuildSolrSearchIndex.PreviousConnectionStatus != ConnectionStatus.Unknown)
-                    {
-                        failResistantSwitchOnRebuildSolrSearchIndex.PreviousConnectionStatus = ConnectionStatus.Failed;
-                        continue;
+                        resistantIndex.ConnectionStatus = ConnectionStatus.Failed;
                     }
                 }
 

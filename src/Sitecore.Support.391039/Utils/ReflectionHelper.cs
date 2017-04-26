23c48d238a27eb0b26ea86ff66454d619e06e329
dk\ivd <ivd@sitecore.net>
2017-04-26 14:37:47 +0800

    #391039: Introduce ReflectionHelper

---
+++ src/Sitecore.Support.391039/Utils/ReflectionHelper.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Sitecore.Support.Utils
+{
+    using System;
+    using System.Reflection;
+
+    public static class ReflectionHelper
+    {
+        public static void SetValueToPrivateField(Type ofType, object target, string fieldName, object value)
+        {
+            var field = ofType.GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+            field.SetValue(target, value);
+        }
+
+        public static object GetValueOfPrivateField(Type ofType, object target, string fieldName)
+        {
+            var field = ofType.GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+            return field.GetValue(target);
+        }
+    }
+}

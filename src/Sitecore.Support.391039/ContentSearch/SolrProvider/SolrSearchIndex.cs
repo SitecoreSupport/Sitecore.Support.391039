dcebd64e3f89fec65ede789f230b17618f60182f
DK\rbi <beardedmanul@gmail.com>
2017-05-29 14:55:37 +0300

    Commit patch

---
+++ src/Sitecore.Support.391039/ContentSearch/SolrProvider/SolrSearchIndex.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.ContentSearch.SolrProvider
+{
+
+    using Sitecore.ContentSearch;
+    using Sitecore.ContentSearch.Maintenance;
+    using Sitecore.ContentSearch.SolrProvider;
+    using Sitecore.Diagnostics;
+    using SolrNet;
+    using SolrNet.Exceptions;
+    using System;
+
+    public class SolrSearchIndex : Sitecore.ContentSearch.SolrProvider.SolrSearchIndex, ISearchIndex, IDisposable
+    {
+        public SolrSearchIndex(string name, string core, IIndexPropertyStore propertyStore) : base(name, core, propertyStore)
+        {
+        }
+
+        public SolrSearchIndex(string name, string core, IIndexPropertyStore propertyStore, string group) : base(name, core, propertyStore, group)
+        {
+        }
+
+        protected internal static bool IsOkSolrConnection()
+        {
+            try
+            {
+                ISolrCoreAdmin solrAdmin = SolrContentSearchManager.SolrAdmin;
+                if (solrAdmin != null)
+                {
+                    solrAdmin.Status();
+                }
+                return true;
+            }
+            catch (SolrConnectionException)
+            {
+                Log.Warn((("SUPPORT: Unable to connect to Solr: " + SolrContentSearchManager.ServiceAddress) ?? "") + "; " + typeof(SolrConnectionException).FullName + " was caught.", new object());
+                return false;
+            }
+        }
+
+        void ISearchIndex.Initialize()
+        {
+            if (IsOkSolrConnection())
+            {
+                base.Initialize();
+            }
+        }
+    }
+
+}
---
+++ src/Sitecore.Support.391039/SolrStatusAgent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support
+{
+    using Sitecore.ContentSearch.SolrProvider;
+    using Sitecore.Diagnostics;
+    using Sitecore.Support.ContentSearch.SolrProvider;
+    using Sitecore.Tasks;
+    using System;
+
+    public class SolrStatusAgent : BaseAgent
+    {
+        private static ConnectionStatus prevConnectionStatus;
+
+        public void Run()
+        {
+            bool flag = Sitecore.Support.ContentSearch.SolrProvider.SolrSearchIndex.IsOkSolrConnection();
+            if ((prevConnectionStatus == ConnectionStatus.Never) & flag)
+            {
+                Log.Warn("SUPPORT: SOLR connection was restored. Indexes are being initialized.", this);
+                SolrContentSearchManager.Initialize();
+                prevConnectionStatus = ConnectionStatus.Ok;
+            }
+            else if ((prevConnectionStatus == ConnectionStatus.No) & flag)
+            {
+                Log.Warn("SUPPORT: SOLR connection was restored.", this);
+                prevConnectionStatus = ConnectionStatus.Ok;
+            }
+            else if ((prevConnectionStatus == ConnectionStatus.Ok) && !flag)
+            {
+                prevConnectionStatus = ConnectionStatus.No;
+            }
+        }
+
+        private enum ConnectionStatus
+        {
+            Never,
+            No,
+            Ok
+        }
+    }
+
+
+}
